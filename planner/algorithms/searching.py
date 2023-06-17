a8bec38ca57df174295b1ce73f210970a9166913
dekompot <dekompot@gmail.com>
2023-06-17 11:19:14 +0200

    ga alpha version: real fitness function

--- planner/algorithms/searching.py
+++ planner/algorithms/searching.py
@@ -34,7 +34,7 @@ def fitness_func(ga_instance, solution, solution_idx):
 
 num_generations = 100  # Number of generations.
 num_parents_mating = 2  # Number of solutions to be selected as parents in the mating pool.
-sol_per_pop = 10  # Number of solutions in the population.
+sol_per_pop = 4  # Number of solutions in the population.
 num_genes = len(courses)
 gene_space = [range(len(course.groups)) for course in courses]
 
@@ -63,7 +63,7 @@ ga_instance = pygad.GA(num_generations=num_generations,
 ga_instance.run()
 
 # ga_instance.plot_fitness()
-
+ga_instance.plot_fitness()
 # Returning the details of the best solution.
 solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
 """print("Parameters of the best solution : {solution}".format(solution=solution))
