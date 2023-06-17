04c8201ab907be9896699b10bdfcc8e56df22001
dekompot <dekompot@gmail.com>
2023-06-17 11:18:11 +0200

    ga alpha version: real fitness function

--- planner/algorithms/searching.py
+++ planner/algorithms/searching.py
@@ -1,5 +1,4 @@
 import pygad
-import numpy as np
 from planner.parsing.parse_json import load_from_json
 from planner.models.groups import Group
 from typing import List
@@ -28,16 +27,18 @@ def is_possible(groups: List[Group]) -> bool:
 def fitness_func(ga_instance, solution, solution_idx):
     sol_groups = translate_solution(solution)
     if is_possible(sol_groups):
-        return 1000
-    else:
         return 0
+    else:
+        return -9999
 
-num_generations = 1  # Number of generations.
-num_parents_mating = 1  # Number of solutions to be selected as parents in the mating pool.
+
+num_generations = 100  # Number of generations.
+num_parents_mating = 2  # Number of solutions to be selected as parents in the mating pool.
 sol_per_pop = 10  # Number of solutions in the population.
 num_genes = len(courses)
-last_fitness = 0
 gene_space = [range(len(course.groups)) for course in courses]
+
+
 def on_generation(ga_instance):
     global last_fitness
     print("Generation = {generation}".format(generation=ga_instance.generations_completed))
@@ -46,6 +47,7 @@ def on_generation(ga_instance):
     print("Change     = {change}".format(
         change=ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1] - last_fitness))
     last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
+
 
 ga_instance = pygad.GA(num_generations=num_generations,
                        num_parents_mating=num_parents_mating,
@@ -71,8 +73,8 @@ for group in translate_solution(solution):
     print(group)
 print('fitness = ', solution_fitness)
 print('solution idx = ', solution_idx)
-#prediction = np.sum(np.array(function_inputs) * solution)
-#print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+# prediction = np.sum(np.array(function_inputs) * solution)
+# print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
 
 """if ga_instance.best_solution_generation != -1:
     print("Best fitness value reached after {best_solution_generation} generations.".format(
